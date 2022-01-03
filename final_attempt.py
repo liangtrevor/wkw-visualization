132c8e1a4b87222237b2f9d422091a35be06d6ac
Trevor Liang <trevorliang005@gmail.com>
2022-01-02 23:23:30 -0800

    filtering actor stage begins

--- final_attempt.py
+++ final_attempt.py
@@ -1,11 +1,11 @@
 from imdb import IMDb
 import pprint
-from typing import List
+from collections import Counter
 
+cc = Counter()
 pp = pprint.PrettyPrinter(indent=1)
 
 ia = IMDb()
-
 
 def movob(keyword):
     subResult = ia.search_movie(keyword)
@@ -24,19 +24,22 @@ filmsList = ['the grandmaster', '2046', 'eros', 'in the mood for love',
         'chungking express', 'days of being wild', 'as tears go by']
 
 filmObjects = []
+filmNames = []
 actors = []
 filmsDict = {}
 
+# if you need to pull id or names, etc -- manipulate this for loop
 for i in filmsList:
+    film = movob(i)
     # search for film and put object into filmObjects list
-    filmObjects.append(movob(i))
+    filmObjects.append(film)
+    # append film name to filmNames. formatted according to imdb
+    filmNames.append(film['title'])
 
 print("\n")
 
 #TODO: get film:list pair by making for loops range(len(...))
 # the intent of this is to get the current film
-
-quit()
 
 # iterate through filmObjects
 for i in filmObjects:
@@ -46,16 +49,28 @@ for i in filmObjects:
     # tempList
     for k in i['actors']:
         # put all actors from current film into actors list
-        actors.append(k['name'])
+        currentNames = k['name']
+        actors.append(currentNames)
+        tempList.append(currentNames)
         # make key:value pair in filmsDict to create film to actor data structure
     currentFilm = i['title']
     filmsDict[currentFilm] = tempList
     # actors.append(i['actors'][0]['name'])
 
-print("actors:")
-print("-----")
-pp.pprint(actors)
+# just for testing
+# print("actors:")
+# print("-----")
+# pp.pprint(actors)
+#
+# print("dictionary:")
+# print("-----")
+# pp.pprint(filmsDict)
 
-print("dictionary:")
-print("-----")
-pp.pprint(filmsDict)
+# TODO:
+#  write a for loop that iterates through the actor list ten times
+# TODO:
+#  make a lambda function that takes out most occuring element
+# TODO:
+#  store that element in a list called recurring_cast
+
+# remove actor duplicates from actors list
